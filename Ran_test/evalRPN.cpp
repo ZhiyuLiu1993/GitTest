346b2b51ff4ca404e3813aea33f7447dc670fdf4
soloyanyi <969070896@qq.com>
2018-05-06 11:02:32 +0800

    evalRPN

---
+++ Ran_test/evalRPN.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <sstream>
+
+
+using namespace std;
+
+class Solution{
+public:
+     int evalRPN(vector<string> &tokens){
+         stack<int> sta;
+         for(const auto &tok: tokens){
+             if(tok == "+" || tok == "-" || tok == "*" || tok == "/"){
+                 int a = sta.top();
+                 sta.pop();
+                 int b = sta.top();
+                 sta.pop();
+                 int c;
+                 if(tok == "+")
+                     c = b + a;
+                 else if(tok == "-")
+                     c = b - a;
+                 else if(tok == "*")
+                     c = b * a;
+                 else if(tok == "/")
+                     c = b / a;
+                 sta.push(c);
+             }
+             else{
+                 stringstream ss;
+                 ss<<tok;
+                 int temp;
+                 ss>>temp;
+                 sta.push(temp);
+             }
+         }
+         return sta.top();
+     }
+};
+
+int main() {
+
+    return (0);
+}
