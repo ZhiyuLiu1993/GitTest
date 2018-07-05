8b9ae931ab0eb589c5395bec13cf816d33c773d8
ZhiyuLiu1993 <969070896@qq.com>
2018-07-05 21:46:35 +0800

    my_rpc Protobuf_base (note protobuf less 3.0)

--- Ran_test/iobuffer.c
+++ Ran_test/iobuffer.c
@@ -3,21 +3,21 @@
   
 int main()  
 {  
-int i=10,pid;  
-  
-printf("nBefor fork.\n");  
-//fflush(stdout);  
-  
-if((pid=fork())<0)  
-{  
-printf("nError fork!");  
-return 1;  
-}  
-else if(pid==0)  
-i++;  
-else  
-sleep(2);  
-  
-printf("ni=%d\n",i);  
-return 0;  
+	int i=10,pid;  
+
+	printf("nBefor fork.\n");  
+	//fflush(stdout);  
+
+	if((pid=fork())<0)  
+	{  
+		printf("nError fork!");  
+		return 1;  
+	}  
+	else if(pid==0)  
+		i++;  
+	else  
+		sleep(2);  
+
+	printf("ni=%d\n",i);  
+	return 0;  
 }
